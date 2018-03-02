43ed18b1047b897f0f072d22cbbec632bee9592e
Natoli <Nicholas.Natoli@mrisoftware.com>
2018-03-02 11:59:35 -0500

    Created a basic container for the game.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public difficulties = [
+    "Beginner",
+    "Intermediate",
+    "Advanced"
+  ];
+
+  startGame() {
+
+  }
 }
